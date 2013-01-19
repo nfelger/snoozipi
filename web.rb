2d709429b9b4230993c6f397f57ec13c96a0c434
Niko Felger <niko.felger@gmail.com>
2013-01-19 22:53:36 +0000

    Most rudimentary webform for setting the alarm time.

--- web.rb
+++ web.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
 get '/' do
+  if params['minute'] && params['hour']
+    File.open('alarms', 'w+') { |file| file << "#{params['hour']}#{params['minute']}"}
+  end
   erb :'index.html'
 end
