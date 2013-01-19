a1bf0b0cad5e922076e8d06bd50a4a631f1cdee9
Niko Felger <niko.felger@gmail.com>
2013-01-19 21:46:04 +0000

    Read alarms FROM A FILE

--- alarm.rb
+++ alarm.rb
@@ -1,6 +1,24 @@
 #!/usr/bin/env ruby
 
-`mpc clear`
-`mpc load "tunes"`
-`mpc shuffle`
-`mpc play`
+def play
+  `mpc clear`
+  `mpc load "tunes"`
+  `mpc shuffle`
+  `mpc play`
+end
+
+while true
+  start = Time.now
+
+  alarms = IO.readlines(File.dirname(__FILE__) + '/alarms')
+  STDERR.puts "Alarms found:\n#{alarms.inspect}"
+
+  STDERR.puts "Current time: #{Time.now.strftime('%H%M%S%L').inspect}"
+  if alarms.any?{|a| a.strip == Time.now.strftime('%H%M')}
+    STDERR.puts 'RRRRRRRRRRRRRRRRRRR'
+    play
+  end
+
+  elapsed = Time.now - start
+  sleep([60 - elapsed, 0].max)
+end
